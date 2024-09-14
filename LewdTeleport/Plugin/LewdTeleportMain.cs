b49679910bb003211e3660bc25520d1905418f78
Neal Robison <nealjrobison@gmail.com>
2024-09-13 20:38:54 -0400

    Added Experimental ENF commands. Formatting changes to fix a broken build.

--- LewdTeleport/Plugin/LewdTeleportMain.cs
+++ LewdTeleport/Plugin/LewdTeleportMain.cs
@@ -3,6 +3,9 @@ using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 using LewdTeleport.Gui;
 using LewdTeleport.Managers;
+using System;
+using System.Security.Cryptography;
+using static FFXIVClientStructs.FFXIV.Client.UI.RaptureAtkHistory.Delegates;
 
 namespace LewdTeleport.Plugin {
     public sealed class LewdTeleportMain : IDalamudPlugin {
@@ -26,8 +29,27 @@ namespace LewdTeleport.Plugin {
 
             PluginInterface.UiBuilder.Draw += OnDraw;
             PluginInterface.UiBuilder.OpenConfigUi += OnOpenConfigUi;
+            ClientState.TerritoryChanged += OnTeleport;
         }
-        
+
+        private void OnTeleport(ushort obj)
+        {
+            Chat.Print("/wstrip -c");
+            Chat.Print("/wstrip - C");
+            Chat.Print("/wait 1");
+            Chat.Print("/examineself motion < wait.1.5 >");
+            Chat.Print("/moodle apply self preset \"ENF\"");
+            Chat.Print("/surprised motion < wait.1.5 >");
+            Chat.Print("/sad motion < wait.1 >");
+            Chat.Print("/snap motion < wait.1.5 >");
+            Chat.Print("/wdress");
+            Chat.Print("/wait 0.5");
+            Chat.Print("/examineself motion < wait.2 >");
+            Chat.Print("/upset motion < wait.1.5 >");
+            Chat.Print("/ppose stand 6");
+            Chat.Print("/porch stop");
+        }
+
         public static void LogChat(string message, bool error = false) {
             switch (error) {
                 case true when Config.ChatError:
